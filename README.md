9bfc00b58b0c5423230c6dc035f999d7c104dde3
Francesco Cartenì <francesco.carteni@gmail.com>
2016-07-29 17:55:36 +0200

    First Commit (paragraphs 1/2/3)

---
+++ README.md
@@ -0,0 +1,4 @@
+mava
+====
+
+A Symfony project created on July 28, 2016, 12:20 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @Route("/about/{name}/", name="aboutpage", defaults={"name":null})
+     * @param $name
+     * @return Response
+     */
+    public  function aboutAction($name)
+    {
+        if($name) {
+            $user = $this->getDoctrine()->getRepository('AppBundle:User')
+              ->findOneBy(array('name'=>$name));
+            if(false === $user instanceof User) {
+                throw $this->createNotFoundException(sprintf('No user named %s found', $name));
+            }
+        }
+
+        return $this->render(':default:about.html.twig',[
+            'user' => $user
+        ]);
+    }
+}
